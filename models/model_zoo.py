b62df474c1d3829240ba7ef2977e9a0e5c1a1500
StephenTerror <2684109034@qq.com>
2021-03-21 17:22:30 +0800

    rename pkg dct_capsnet as tss_capsnet
    Signed-off-by: StephenTerror <2684109034@qq.com>

--- models/model_zoo.py
+++ models/model_zoo.py
@@ -1,4 +1,4 @@
-from .dct_capsnet import FFTCapsNet
-from .dct_capsnet import FFTEfficientCapsNet
+from .tss_capsnet import FFTCapsNet
+from .tss_capsnet import FFTEfficientCapsNet
 from .efficient_capsnet import EfficientCapsNet
 from .origin_capsnet import CapsNet
