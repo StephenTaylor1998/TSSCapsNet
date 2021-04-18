9286d5651836fbdad103a6487a0e79f18ae1f053
StephenTerror <2684109034@qq.com>
2021-04-18 22:59:02 +0800

    cifar10.sh
    Signed-off-by: StephenTerror <2684109034@qq.com>

---
+++ tutorial/research/cifar10.sh
@@ -0,0 +1,36 @@
+python main.py --arch ETCModel --model-name DWT_Tiny_Half_R18_Tiny_FPN_CIFAR \
+--data-name CIFAR10 --initial-epoch 0 --select-gpu 1 --test True \
+>DWT_Tiny_Half_R18_Tiny_FPN_CIFAR.txt
+
+python main.py --arch ETCModel --model-name DWT_Tiny_Half_R18_FPN_CIFAR \
+--data-name CIFAR10 --initial-epoch 0 --select-gpu 2 --test True \
+>DWT_Tiny_Half_R18_FPN_CIFAR.txt
+
+python main.py --arch ETCModel --model-name DWT_Tiny_Half_R18_Attention_FPN_CIFAR \
+--data-name CIFAR10 --initial-epoch 0 --select-gpu 3 --test True \
+>DWT_Tiny_Half_R18_Attention_FPN_CIFAR.txt
+
+python main.py --arch ETCModel --model-name DWT_Tiny_Half_R34_Tiny_FPN_CIFAR \
+--data-name CIFAR10 --initial-epoch 0 --select-gpu 4 --test True \
+>DWT_Tiny_Half_R34_Tiny_FPN_CIFAR.txt
+
+python main.py --arch ETCModel --model-name DWT_Tiny_Half_R34_FPN_CIFAR \
+--data-name CIFAR10 --initial-epoch 0 --select-gpu 5 --test True \
+>DWT_Tiny_Half_R34_FPN_CIFAR.txt
+
+python main.py --arch ETCModel --model-name DWT_Tiny_Half_R34_Attention_FPN_CIFAR \
+--data-name CIFAR10 --initial-epoch 0 --select-gpu 6 --test True \
+>DWT_Tiny_Half_R34_Attention_FPN_CIFAR.txt
+
+python main.py --arch ETCModel --model-name DWT_Tiny_Half_R50_Tiny_FPN_CIFAR \
+--data-name CIFAR10 --initial-epoch 0 --select-gpu 7 --test True \
+>DWT_Tiny_Half_R50_Tiny_FPN_CIFAR.txt
+
+python main.py --arch ETCModel --model-name DWT_Tiny_Half_R50_FPN_CIFAR \
+--data-name CIFAR10 --initial-epoch 0 --select-gpu 8 --test True \
+>DWT_Tiny_Half_R50_FPN_CIFAR.txt
+
+python main.py --arch ETCModel --model-name DWT_Tiny_Half_R50_Attention_FPN_CIFAR \
+--data-name CIFAR10 --initial-epoch 0 --select-gpu 9 --test True \
+>DWT_Tiny_Half_R50_Attention_FPN_CIFAR.txt
+
